512ac26946e79e27d85ce6018f32a93419bf8a5c
WillianApMachado <willianapmg@gmail.com>
2025-06-26 00:08:27 -0300

    Update main.py
    Alteração no codigo de main.py

--- Main.py
+++ Main.py
@@ -5,27 +5,33 @@ import sys
 #Classe de objettots da janela principal
 class Ui_mainWindow(QMainWindow):
     #Construtor da classe
-    def __init__(self,):
+    def __init__(self,):    
+        #   Chama o construtor da classe pai QMainWindow
         super(Ui_mainWindow, self).__init__()
+        #Configurações da janela
         self.setWindowTitle ("Primeira Janela do Chatbot")
         self.setGeometry(100, 100, 600, 400)
-        
         self.setupUi()
-    def setupUi(self): 
-        self.text = QLabel(self)
-        
-        self.text.move(50, 50)   
 
+    # Método para configurar os elementos da interface    
+    def setupUi(self): 
+        # Cria um campo de texto
+        self.text = QLabel(self)
+        self.text.move(50, 50)   
+        # Cria botões
         self.btn1 = QtWidgets.QPushButton(self)
         self.btn1.setText('Botão 1') 
         self.btn1.clicked.connect(self.on_click)
+        
     def on_click(self):
+        # Ação a ser executada quando o botão for clicado
         print('Botão clicado!')
         self.btn1.move(50, 100)
         self.text.setText(' Esse texto foi alteradoaaaaaaaaaaaaaaaa')   
         self.text.adjustSize()
 
-def main(): 
+def main():
+    # Cria uma instância da aplicação 
     app = QApplication(sys.argv)
     Window = Ui_mainWindow()  
    
